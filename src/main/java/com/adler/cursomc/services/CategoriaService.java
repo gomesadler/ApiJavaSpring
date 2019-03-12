7174b8baf55b15c15130c54d3a52bab37f080820
gomesadler@hotmail.com <adler.sa@segep.ma.gov.br>
2019-03-11 23:36:51 -0300

    Criando o repository e service para o domain Categoria

---
+++ src/main/java/com/adler/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.adler.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adler.cursomc.domain.Categoria;
+import com.adler.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);	
+	}
+	
+	
+}
